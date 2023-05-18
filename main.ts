7b4b4f8ed78db8380952cbeb75d2f00bf69c7ccd
vgav855 <132547267+vgav855@users.noreply.github.com>
2023-05-18 09:05:35 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Confused)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+basic.showIcon(IconNames.Asleep)
